6f4ecdd2dcffd6cacab66172ed52bb3beeb95e34
Greg Miller <hypurban86@gmail.com>
2025-07-14 10:17:56 -0400

    updates base path in vite cfg

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
 
-	base: '/ThreeQuery/',
+	base: '/Vue-Window-Manager/',
 
 	plugins: [
 		vue(),
